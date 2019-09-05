33f98358a79069e0b3b456659c3a30d7e21ab116
ralex <ralex.rdz@gmail.com>
2019-09-05 18:23:49 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let name
+	console.log('App')
+	import Playground from './components/Playground.svelte';
+	const pointsArray = [[[5, 10], [10, 40], [40, 30], [60, 5], [90, 45], [120, 10], [150, 45], [200, 10]]]
+</script>
+
+<style>
+</style>
+
+<Playground pointsArray={pointsArray}/>
+
+
+
---
+++ src/components/Playground.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Shape from './Shape.svelte'
+  export let w
+  export let h
+  export let pointsArray
+</script>
+
+<h1>Playground</h1>
+<svg width={w} height={h}>
+  {#each pointsArray as points}
+    <Shape points={points}/>
+  {/each}
+</svg>
